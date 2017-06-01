9f549e5b4698b13e5d08a273ae66977711a354dd
zhehui <zhehui.zhou@outlook.com>
2017-05-31 23:35:18 -0400

    init, data class, permissions

---
+++ app/src/main/java/com/example/zhehui/newsapp/Story.java
@@ -0,0 +1,34 @@
+package com.example.zhehui.newsapp;
+
+public class Story {
+    /**
+     * Story information member fields.
+     */
+    private String mTitle;
+    private String mSection;
+    private String mWebURL;
+
+    /**
+     * Constructor.
+     */
+    public Story(String title, String section, String url) {
+        mTitle = title;
+        mSection = section;
+        mWebURL = url;
+    }
+
+    /**
+     * Getters.
+     */
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getSection() {
+        return mSection;
+    }
+
+    public String getWebURL() {
+        return mWebURL;
+    }
+}
