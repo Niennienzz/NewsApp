bdee4316cdca8a30b30339bf18056b5d383dc05c
zhehui <zhehui.zhou@outlook.com>
2017-06-01 00:29:53 -0400

    clean up, add alt colors for list items

--- app/src/main/java/com/example/zhehui/newsapp/NewsAdapter.java
+++ app/src/main/java/com/example/zhehui/newsapp/NewsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.zhehui.newsapp;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,12 +28,21 @@ public class NewsAdapter extends ArrayAdapter<Story> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Inflate list item view.
         View listItemView = convertView;
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.story_item, parent, false);
         }
 
+        // Alternative background colors.
+        if (position % 2 == 1) {
+            listItemView.setBackgroundColor(Color.CYAN);
+        } else {
+            listItemView.setBackgroundColor(Color.WHITE);
+        }
+
+        // Populate data.
         Story currentStory = getItem(position);
 
         TextView titleView = (TextView) listItemView.findViewById(R.id.story_title);
